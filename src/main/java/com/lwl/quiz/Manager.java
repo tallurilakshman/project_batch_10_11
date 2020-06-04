2e09bb15e8dabd765c493b5ad643b75df954d654
Lakshman <tanvi.avula@outlook.com>
2020-06-04 23:36:28 +0530

    Added JSON format methods

--- src/main/java/com/lwl/quiz/Manager.java
+++ src/main/java/com/lwl/quiz/Manager.java
@@ -1,27 +1,49 @@
 package com.lwl.quiz;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
-class Employees{
+class Employees {
 	private List<Person> employees;
 }
+
 @Getter
 @Setter
-class Person{
+class Person {
 	String name;
 	String email;
+
 	private Person() {
-		
+
 	}
 }
+
 public class Manager {
 	public static void main(String[] args) {
-		QuizService service = new QuizServiceImpl();
-		service.startQuiz();
-		
+
+		String data = "{\"employees\":[{\"name\":\"krish\",\"email\":\"krish@gmail.com\"},{\"name\":\"nag\",\"email\":\"nag@gmail.com\"}]}";
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Employees person = mapper.readValue(data, Employees.class);
+			person.getEmployees().forEach(e -> {
+				System.out.println(e.getName());
+			});
+			String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(person);
+			System.out.println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+//		QuizService quizService = new QuizServiceImpl();
+//		quizService.startQuiz();
+
 	}
 }
